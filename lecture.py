688932a16b64de095123a2c3470857364d8d3a17
lololepro17 <123694004+lololepro17@users.noreply.github.com>
2024-11-06 10:45:15 +0100

    Update lecture.py

--- lecture.py
+++ lecture.py
@@ -7,3 +7,9 @@ def save_grid(grille, nom_fichier):
 def load_grid(nom_fichier):
     with open(nom_fichier, 'r') as f: # Open file with permission of read.
         return json.load(f)
+
+def afficher_grille(grille):
+    for ligne in grille:
+        print(" ".join(str(val) if val != 0 else "." for val in ligne)) 
+
+afficher_grille(load_grid("empty_grid.json"))
